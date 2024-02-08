d021c55fdff6794eb690245a50f5ad54fc85a674
kevin funck <kevin.funck@web.de>
2024-02-08 12:03:19 +0100

    first

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"bossel","appId":"1:1075134453561:web:f57f0d4cb8816e44b73ae8","storageBucket":"bossel.appspot.com","apiKey":"AIzaSyB0l-UgqD5Qc5I4NifphLlJh4V3ewMXrdc","authDomain":"bossel.firebaseapp.com","messagingSenderId":"1075134453561"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
